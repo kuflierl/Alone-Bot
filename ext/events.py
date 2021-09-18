f18eb0184890f00939a29726adea8a8fff5a3933
NightSlasher35 <49624805+NightSlasher35@users.noreply.github.com>
2021-09-18 16:43:56 +0100

    Update events.py

--- ext/events.py
+++ ext/events.py
@@ -5,7 +5,7 @@ class Events(commands.Cog):
   def __init__(self, bot):
    self.bot = bot
   
-  @bot.listen("on_message")
+  @commands.Cog.listener("on_message")
   async def prefixcheck(message):
    if message.content == "!a" or message.content == "•" and not message.author.bot:
     await message.reply(embed=generate_embed("Roger that", "Hey you actually have to use a command ok thanks", "Alone Bot"), mention_author=False)
@@ -18,8 +18,6 @@ class Events(commands.Cog):
   async def on_message(self, message):
    if message.content == "<@784545186612510811>" or message.content == "@Alone Bot#5952" and not message.author.bot:
     await message.reply(embed=generate_embed("Roger that", "Hey you actually have to use a command ok thanks", "Alone Bot"), mention_author=False)
-
-
 
 def setup(bot):
   bot.add_cog(Events(bot))
