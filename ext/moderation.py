ae2fa6b3df32a8501765200cd56d3351ff54608a
kuflierl <41301536+kuflierl@users.noreply.github.com>
2022-05-13 17:45:54 +0200

    Refactor a lot, add __main__.py, add requirements.txt

--- ext/moderation.py
+++ ext/moderation.py
@@ -3,11 +3,11 @@ from discord.ext import commands
 
 class Moderation(commands.Cog):
   def __init__(self, bot):
-  	self.bot = bot
+    self.bot = bot
 
   async def cog_check(self, ctx):
    if not ctx.guild:
-   	return False
+    return False
    return True
 
   @commands.command()
@@ -15,35 +15,35 @@ class Moderation(commands.Cog):
   @commands.has_guild_permissions(ban_members=True)
   async def ban(self, ctx, member: discord.Member = None, *, reason: str = None):
    if member == None:
-   	await ctx.reply(embed=generate_embed("An error occured", "You need to provide a member to ban."))
+    await ctx.reply(embed=generate_embed("An error occured", "You need to provide a member to ban."))
    elif reason == None:
-   	await member.ban(reason="No reason provided.")
-   	await ctx.reply(f"Banned {member} for no reason.")
+    await member.ban(reason="No reason provided.")
+    await ctx.reply(f"Banned {member} for no reason.")
    else:
-   	await member.ban(reason=reason)
-   	await ctx.reply(f"Banned {member} for {reason}.")
+    await member.ban(reason=reason)
+    await ctx.reply(f"Banned {member} for {reason}.")
 
   @commands.command()
   @commands.bot_has_guild_permissions(kick_members=True)
   @commands.has_guild_permissions(kick_members=True)
   async def kick(self, ctx, member: discord.Member = None, *, reason: str = None):
    if member == None:
-   	await ctx.reply(embed=generate_embed("An error occured", "You need to provide a member to kick."))
+    await ctx.reply(embed=generate_embed("An error occured", "You need to provide a member to kick."))
    elif reason == None:
-   	await member.kick(reason="No reason provided.")
-   	await ctx.reply(f"Kicked {member} for no reason.")
+    await member.kick(reason="No reason provided.")
+    await ctx.reply(f"Kicked {member} for no reason.")
    else:
-   	await member.kick(reason=reason)
-   	await ctx.reply(f"Kicked {member} for {reason}.")
+    await member.kick(reason=reason)
+    await ctx.reply(f"Kicked {member} for {reason}.")
 
   @commands.command()
   @commands.bot_has_guild_permissions(manage_messages=True)
   @commands.has_guild_permissions(manage_messages=True)
   async def purge(self, ctx, limit: int=None):
    if not limit:
-   	limit = 20
+    limit = 20
    messages = await ctx.channel.purge(limit=limit)
    await ctx.send(f"{len(messages)} messages deleted.", delete_after=15)
 
 def setup(bot):
-	bot.add_cog(Moderation(bot))
+    bot.add_cog(Moderation(bot))
--- main.py
+++ main.py
@@ -3,114 +3,152 @@ from discord.ext import commands, tasks
 from datetime import datetime
 from typing import Optional
 
+
 class Help(commands.HelpCommand):
-	 def get_command_signature(self, command):
-	 	return "%s %s" % (command.qualified_name, command.signature)
+    def get_command_signature(self, command):
+        return "%s %s" % (command.qualified_name, command.signature)
 
-	 async def send_bot_help(self, mapping):
-	 	embed = discord.Embed(title="Help", description="Use help (command) or help (category) for more information\n <> is a required argument | [] is an optional argument", color=discord.Color.blurple())
-	 	embed.set_footer(text=f"Command ran by {self.context.author.display_name}", icon_url=self.context.author.avatar_url)
-	 	embed.timestamp = datetime.utcnow()
-	 	for cog, command in mapping.items():
-	 		filtered = await self.filter_commands(command, sort=True)
-	 		command_signatures = [self.get_command_signature(c) for c in filtered]
-	 		if command_signatures:
-	 			cog_name = getattr(cog, "qualified_name", "No Category")
-	 			embed.add_field(name=cog_name, value=" ".join(command_signatures), inline=False)
-	 	await self.context.send(embed=embed)
+    async def send_bot_help(self, mapping):
+        embed = discord.Embed(
+            title="Help",
+            description="Use help (command) or help (category) for more information\n <> is a required argument | [] is an optional argument",
+            color=discord.Color.blurple(),
+        )
+        embed.set_footer(
+            text=f"Command ran by {self.context.author.display_name}",
+            icon_url=self.context.author.avatar_url,
+        )
+        embed.timestamp = datetime.utcnow()
+        for cog, command in mapping.items():
+            filtered = await self.filter_commands(command, sort=True)
+            command_signatures = [self.get_command_signature(c) for c in filtered]
+            if command_signatures:
+                cog_name = getattr(cog, "qualified_name", "No Category")
+                embed.add_field(
+                    name=cog_name, value=" ".join(command_signatures), inline=False
+                )
+        await self.context.send(embed=embed)
 
-	 async def send_command_help(self, command):
-	 	embed = discord.Embed(title=self.get_command_signature(command))
-	 	embed.add_field(name="Description of the command", value=command.help)
-	 	alias = command.aliases
-	 	if alias:
-	 		embed.add_field(name="Aliases", value=", ".join(alias), inline=False)
+    async def send_command_help(self, command):
+        embed = discord.Embed(title=self.get_command_signature(command))
+        embed.add_field(name="Description of the command", value=command.help)
+        alias = command.aliases
+        if alias:
+            embed.add_field(name="Aliases", value=", ".join(alias), inline=False)
 
-	 async def send_error_message(self, error):
-	 	embed = discord.Embed(title="Error", description=error)
-	 	await self.context.send(embed=embed)
+    async def send_error_message(self, error):
+        embed = discord.Embed(title="Error", description=error)
+        await self.context.send(embed=embed)
 
-	 async def send_group_help(self, group):
-	 	await self.context.send("This is help group")
+    async def send_group_help(self, group):
+        await self.context.send("This is help group")
 
-	 async def send_cog_help(self, cog):
-	 	embed = discord.Embed(title=cog.qualified_name, description=cog.description)
-	 	embed.add_field(name="Commands", value="\n".join(cog.get_commands()))
+    async def send_cog_help(self, cog):
+        embed = discord.Embed(title=cog.qualified_name, description=cog.description)
+        embed.add_field(name="Commands", value="\n".join(cog.get_commands()))
 
-os.environ["JISHAKU_HIDE"] = "true"
-os.environ["JISHAKU_RETAIN"] = "true"
-os.environ["JISHAKU_NO_UNDERSCORE"] = "true"
-os.environ["JISHAKU_FORCE_PAGINATOR"] = "true"
-os.environ["JISHAKU_NO_DM_TRACEBACK"] = "true"
 
-#class DeleteButton(discord.Button):
-#	def __init__(self):
-#		self.style = discord.ButtonStyle.red()
-#		self.label = "Delete"
+# class DeleteButton(discord.Button):
+#    def __init__(self):
+#        self.style = discord.ButtonStyle.red()
+#        self.label = "Delete"
+
 
 class AloneBot(commands.AutoShardedBot):
-  	def __init__(self, *args, **kwargs):
-  		super().__init__(command_prefix=("!a", "•"), *args, **kwargs)
-  		self.token = os.getenv("token")
-		self.blacklist: dict[int, str] = {}
-  		self.afk: dict[int, str] = {}
-  		self.maintenance = False
-  		self.maintenance_reason = ""
-  		self.db = 0
-  		self.activity = discord.Game("with my Source Code")
-  		self.owner_ids = [349373972103561218, 412734157819609090, 755055117773963476]
-  		self.command_counter = 0
-  		self.launch_time = datetime.utcnow()
-  		self.support_server = "https://discord.gg/kFkAmqhm"
-  		self.strip_after_prefix = True
-  		self.case_insensitive = True
+    def __init__(self, *args, **kwargs):
+        super().__init__(command_prefix=("!a", "•"), *args, **kwargs)
+        self.token = os.getenv("token")
+        self.blacklist: dict[int, str] = {}
+        self.afk: dict[int, str] = {}
+        self.maintenance = False
+        self.maintenance_reason = ""
+        self.db = 0
+        self.activity = discord.Game("with my Source Code")
+        self.owner_ids = [349373972103561218, 412734157819609090, 755055117773963476]
+        self.command_counter = 0
+        self.launch_time = datetime.utcnow()
+        self.support_server = "https://discord.gg/kFkAmqhm"
+        self.strip_after_prefix = True
+        self.case_insensitive = True
 
-  	def generate_embed(self, ctx: commands.Context, title: str, content: str, color: Optional[int] = None):
-  		embed = discord.Embed()
-  		embed.title = title
-  		embed.description = content
-  		if color is None:
-  			embed.color = discord.Color.random()
-  		else:
-  			embed.color = int(color, 16)
-  			embed.set_footer(text=f"Command ran by {ctx.author.display_name}", icon_url=ctx.author.avatar_url)
-  			embed.timestamp = datetime.utcnow()
-  			return embed
+    def generate_embed(
+        self,
+        ctx: commands.Context,
+        title: str,
+        content: str,
+        color: Optional[int] = None,
+    ):
+        embed = discord.Embed()
+        embed.title = title
+        embed.description = content
+        if color is None:
+            embed.color = discord.Color.random()
+        else:
+            embed.color = int(color, 16)
+            embed.set_footer(
+                text=f"Command ran by {ctx.author.display_name}",
+                icon_url=ctx.author.avatar_url,
+            )
+            embed.timestamp = datetime.utcnow()
+            return embed
 
-  	def botmsgs(self, msg):
-  		return msg.author == self.user
+    def botmsgs(self, msg):
+        return msg.author == self.user
+
 
 class BlacklistedError(commands.CheckFailure):
-	pass
+    pass
+
+
 class MaintenanceError(commands.CheckFailure):
-	pass
+    pass
 
-bot = AloneBot(intents=discord.Intents.all())
 
-bot.load_extension("jishaku")
-initial_extensions = ["ext.error", "ext.events", "ext.fun", "ext.help", "ext.moderation", "ext.owner", "ext.utility"]
-for cog in initial_extensions:
-	bot.load_extension(cog)
+def main():
+    os.environ["JISHAKU_HIDE"] = "true"
+    os.environ["JISHAKU_RETAIN"] = "true"
+    os.environ["JISHAKU_NO_UNDERSCORE"] = "true"
+    os.environ["JISHAKU_FORCE_PAGINATOR"] = "true"
+    os.environ["JISHAKU_NO_DM_TRACEBACK"] = "true"
 
-@bot.after_invoke
-async def aftercount(ctx):
-	bot.command_counter += 1
+    bot = AloneBot(intents=discord.Intents.all())
 
-@bot.check
-def blacklist(ctx):
-  if ctx.author.id not in bot.blacklist:
-  	return True
-  else:
-  	raise BlacklistedError
+    bot.load_extension("jishaku")
+    initial_extensions = [
+        "ext.error",
+        "ext.events",
+        "ext.fun",
+        "ext.help",
+        "ext.moderation",
+        "ext.owner",
+        "ext.utility",
+    ]
+    for cog in initial_extensions:
+        bot.load_extension(cog)
 
-@bot.check
-def maintenance(ctx):
-	if bot.maintenance is False:
-		return True
-	else:
-		if ctx.author.id in bot.ownerids:
-			return True
-		else:
-			raise MaintenanceError
+    @bot.after_invoke
+    async def aftercount(ctx):
+        bot.command_counter += 1
 
-bot.run(bot.token)
+    @bot.check
+    def blacklist(ctx):
+        if ctx.author.id not in bot.blacklist:
+            return True
+        else:
+            raise BlacklistedError
+
+    @bot.check
+    def maintenance(ctx):
+        if bot.maintenance is False:
+            return True
+        else:
+            if ctx.author.id in bot.ownerids:
+                return True
+            else:
+                raise MaintenanceError
+
+    bot.run(bot.token)
+
+
+if __name__ == "__main__":
+    main()
