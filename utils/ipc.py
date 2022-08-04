d020424f60ddfac3729fecd0a22bfb72fdf3f02a
NightSlasher35 <49624805+NightSlasher35@users.noreply.github.com>
2022-08-04 15:31:10 +0100

    kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk

--- utils/ipc.py
+++ utils/ipc.py
@@ -12,3 +12,18 @@ IPC = Client(
     port=2300, 
     secret_key=os.getenv("ipc_key"),
 )
+
+@app.route('/')
+async def main():
+    return await app.ipc.request("get_user_data", user_id=383946213629624322)
+
+if __name__ == '__main__':
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    try:
+        app.ipc = loop.run_until_complete(IPC.start(loop=loop))
+        app.run(loop=loop)
+    finally:
+        loop.run_until_complete(app.ipc.close())
+        loop.close()
