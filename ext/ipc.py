c699ea62b7c6013adec1f1f3c971ec2a81e6b30d
NightSlasher35 <49624805+NightSlasher35@users.noreply.github.com>
2022-08-06 00:22:47 +0100

    a

--- ext/ipc.py
+++ ext/ipc.py
@@ -17,15 +17,7 @@ class IPC(commands.Cog):
     async def get_user_data(self, data):
         user = self.bot.get_user(data.user_id)
         return user._to_minimal_user_json()
-    
-    async def cog_unload(bot):
-        format = self.bot.format_print("IPC")
-        bot.ipc_online = True if bot.ipc else False
-        if bot.ipc:
-            bot.ipc = None
-            print(f"{format} | Disconnected")
-        print("{format} | Unloaded")
-    
+
     async def cog_load(bot):
         if not hasattr(bot, "ipc"):
             bot.ipc = ipc.Server(self.bot, host="127.0.0.1", port=2300, secret_key=os.getenv("ipc_key"))
